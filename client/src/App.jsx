3248c90e87fa3b2987739c976c442ad2c194abf3
iihimanshu <iihimanshu.b2@hotmail.com>
2024-11-11 11:44:21 +0530

    fe changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import {Routes,Route} from 'react-router-dom';
+import Auth from './pages/Login';
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route path='/login' element={<Auth type='login'/>} />
+        <Route path='/register' element={<Auth type='register'/>} />
+        {/* <Route path='/register' element={<Login/>} />
+        <Route path='/home' element={<Login/>} /> */}
+
+      </Routes>
     </>
   )
 }
